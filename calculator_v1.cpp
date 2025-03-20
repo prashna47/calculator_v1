2d0dfeb03ba4c30d8622ae17ffcb827e0aeed52b
Prashna <sthaprashna047@gmail.com>
2025-03-20 14:30:26 +0545

    First file added1

---
+++ calculator_v1.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void getValidNumber(){
+
+}
+
+int main(){
+    getValidNumber(num1);
+    cout << "Enter operatiom (+, -, /, *):";
+    cout << "Enter second number"
+    double num1, num2, result;
+
+    switch (operation){
+        case '/' :
+        if(num2==0){
+            cout << "Error." <<end1;
+            return 1;
+        }  
+        result = num1/num2;
+        break;
+    }
+
+}
